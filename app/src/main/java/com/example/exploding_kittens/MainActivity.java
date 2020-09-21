f542bf8944350dbcc2bfacd1b15a170467d1ca38
SamWarrick <45551897+SamWarrick@users.noreply.github.com>
2020-09-21 16:46:07 -0700

    First drawing of an actual card

--- app/src/main/java/com/example/exploding_kittens/MainActivity.java
+++ app/src/main/java/com/example/exploding_kittens/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.exploding_kittens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SurfaceView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardView = (SurfaceView) findViewById(R.id.surfaceview);
     }
 }
