f58b8da19de51e4de8b9efcff712d33399d928fd
Chandler <lauc21@up.edu>
2020-09-21 17:10:03 -0700

    first image button card thing

--- app/src/main/java/com/example/exploding_kittens/MainActivity.java
+++ app/src/main/java/com/example/exploding_kittens/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cardView = (SurfaceView) findViewById(R.id.surfaceview);
+        //cardView = (SurfaceView) findViewById(R.id.surfaceview);
     }
 }
